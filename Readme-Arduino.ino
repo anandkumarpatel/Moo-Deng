e988f5d3a02898ff0848f25b77aed1c25144c42b
Anandkumar Patel <anandkumarpatel@users.noreply.github.com>
2024-09-18 22:44:57 -0700

    Update Readme-Arduino.ino

--- Readme-Arduino.ino
+++ Readme-Arduino.ino
@@ -1,11 +1,9 @@
 #include <Keyboard.h>
-const int trigPin = 9;
-const int echoPin = 10;
+
 bool isGameStart = false;
-const int TARGET_DISTANCE = 10;
-const int DISTANCE_DELTA = 2;
 const int TARGET_TEMPERATURE = 420;
 const int TARGET_WATER_LEVEL = 150;
+const int TARGET_LIGHT = 50;
 const int tempPin = A5;
 bool tempDone = false;
 const int waterPin = A0;
@@ -13,45 +11,65 @@ bool waterDone = false;
 const int soundPin = 2;
 bool soundDone = false;
 const int coinPin = 8;
+const int lightPin = A4;
 
-const int movePin = 10;
 bool isMoving = false;
-bool moveDone = false;
+bool lightDone = false;
+
+const int led1 = 12;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(movePin, INPUT);
   pinMode(coinPin, INPUT_PULLUP);
+  pinMode(led1, OUTPUT);
+
+  isGameStart = false;
+  tempDone = false;
+  waterDone = false;
+  soundDone = false;
+  isMoving = false;
+  lightDone = false;
+
+
   Keyboard.begin();
 }
 void loop() {
+
   if (!isGameStart && digitalRead(coinPin) == LOW) {
-    // Keyboard.write(32);
+    Keyboard.write(32);
     Serial.println("space");
     isGameStart = true;
-    delay(100);
+    digitalWrite(led1, HIGH);
+    delay(1000);
    }
+
   if(!isGameStart) {
     return;
   }
-  int temperature = analogRead(tempPin);
-  // Serial.print("temperature: ");
-  // Serial.println(temperature);
-  if (!tempDone && temperature <= TARGET_TEMPERATURE) {
-    tempDone = true;
-    Serial.println("temp reached");
+
+  if (!tempDone) {
+    int temperature = analogRead(tempPin);
+    // Serial.print("temperature: ");
+    // Serial.println(temperature);
+    if (temperature <= TARGET_TEMPERATURE) {
+      tempDone = true;
+      Serial.println("temp reached");
+    }
   }
-  int waterLevel = analogRead(waterPin);
-  // Serial.print("waterLevel: ");
-  // Serial.println(waterLevel);
-  if (!waterDone && waterLevel <= TARGET_WATER_LEVEL) {
-    waterDone = true;
-    Serial.println("water reached");
-  } else {
-    waterDone = false;
+
+  // unsigned long currentTime = millis();
+  // Serial.print("Current time: ");
+  // Serial.println(currentTime);
+  if (!waterDone) {
+    int waterLevel = analogRead(waterPin);
+    // Serial.print("waterLevel: ");
+    // Serial.println(waterLevel);
+    if (waterLevel <= TARGET_WATER_LEVEL) {
+      waterDone = true;
+      Serial.println("water reached");
+    }
   }
+
   int soundHeard = digitalRead(soundPin);
   if(soundHeard == HIGH){
     Serial.println("sound");
@@ -60,26 +78,22 @@ void loop() {
     soundDone = false;
   }
 
-  int moveVal = digitalRead(movePin);  
-  if (moveVal == HIGH) {  
-    if (isMoving == false) { 
-      Serial.println("Motion detected");  
-      isMoving = true;
-      moveDone = true;
-    } 
-  } else { 
-    if (isMoving){ 
-      Serial.println("The motion has stopped"); 
-      isMoving = false;  
-    } 
-  } 
 
-  if (moveDone && tempDone && waterDone && soundDone) {
+  if (!lightDone) {
+    int lightValue = analogRead(lightPin);
+    if (lightValue < TARGET_LIGHT) {
+    lightDone = true;
+      Serial.println("light reached");
+    }
+  }
+
+  if (lightDone && tempDone && waterDone && soundDone) {
     Keyboard.write(10);
     Serial.println("konami");
     tempDone = false;
     waterDone = false;
     soundDone = false;
+    lightDone = false;
     delay(30000);
     Serial.println("konami done");
   }
